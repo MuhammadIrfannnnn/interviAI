b46a1cfff2a4f623706221bb8bd7765f5623dd9d
Hasan Arsalan <hasanarsalan781@gmail.com>
2026-07-26 20:42:40 +0500

    Added main.py with frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,12 +4,19 @@ from app.api.v1.auth import router as auth_router
 from app.api.v1.users import router as users_router
 from app.api.v1.resume import router as resume_router
 from app.api.v1.interview import router as interview_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # your Vite dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def wlcm():
